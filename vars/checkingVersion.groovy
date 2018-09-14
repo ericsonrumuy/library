ef8f12fb11ecba82f3ecc70299e40fec6c019937
Ericson Rumuy <ericson.rumuy@gdn-commerce.com>
2018-09-14 13:56:09 +0700

    update cheking version

--- vars/checkingVersion.groovy
+++ vars/checkingVersion.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(String SERVICE, String ENVIRONMENT){
-    mkdir -p /var/lib/jenkins/gdn_nonprod_chef_repo/data_bags/${SERVICE}
+    mkdir -p /var/lib/jenkins/gdn_nonprod_chef_repo/data_bags/gdn_app_zurich
     echo "************************************"
     echo "Downloading existing Data bag"
     echo "************************************"
-    knife data bag show ${SERVICE} ${ENVIRONMENT} --secret-file /var/lib/jenkins/gdn_nonprod_chef_repo/nonprod_encrypted_data_bag_secret -Fj > /var/lib/jenkins/gdn_nonprod_chef_repo/data_bags/${SERVICE}/${ENVIRONMENT}.json
+    knife data bag show gdn_app_zurich gdn-perf --secret-file /var/lib/jenkins/gdn_nonprod_chef_repo/nonprod_encrypted_data_bag_secret -Fj > /var/lib/jenkins/gdn_nonprod_chef_repo/data_bags/gdn_app_zurich/gdn-perf.json
 }
