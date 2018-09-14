6cfd4b885da2ca96a2b1320bd8e9154fe9387ab6
Ericson Rumuy <ericson.rumuy@gdn-commerce.com>
2018-09-14 13:29:25 +0700

    update cheking version

--- vars/checkingVersion.groovy
+++ vars/checkingVersion.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(){ 
-    mkdir -p /var/lib/jenkins/gdn_nonprod_chef_repo/data_bags/gdn_app_zurich
     echo "************************************"
     echo "Downloading existing Data bag"
     echo "************************************"
-    knife data bag show gdn_app_zurich gdn-perf --secret-file /var/lib/jenkins/gdn_nonprod_chef_repo/nonprod_encrypted_data_bag_secret -Fj > /var/lib/jenkins/gdn_nonprod_chef_repo/data_bags/gdn_app_zurich/gdn-perf.json'''
 }
