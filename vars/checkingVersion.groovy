32c6f6aea016b240fcec2c50cc04201615effe54
Ericson Rumuy <ericson.rumuy@gdn-commerce.com>
2018-09-14 13:43:32 +0700

    update cheking version

--- vars/checkingVersion.groovy
+++ vars/checkingVersion.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String nama){ 
+def call(){ 
     echo "************************************"
     echo "Downloading existing Data bag, ${nama}"
     echo "************************************"
