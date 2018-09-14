ea4ddae9bfd6cdc9b8e5b2bcf5f0dfd5169341ed
Ericson Rumuy <ericson.rumuy@gdn-commerce.com>
2018-09-14 14:20:43 +0700

    update cheking version

--- vars/checkingVersion.groovy
+++ vars/checkingVersion.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
-def call(){
-    sh "mkdir -p /var/lib/jenkins/gdn_nonprod_chef_repo/data_bags/tesdatabag"
+def call(String SERVICE){
+    sh "mkdir -p /var/lib/jenkins/gdn_nonprod_chef_repo/data_bags/${SERVICE}"
     echo "************************************"
     echo "Downloading existing Data bag"
     echo "************************************"
